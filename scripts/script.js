f5306eb959bb36c58ff1618776a9956d26258299
Łukasz Gnyra <lukaszgnyra@outlook.com>
2020-05-10 18:47:47 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,18 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
+  let i;
+  const slides = document.getElementsByClassName("slides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -30,12 +29,8 @@ function showSlides(n) {
 
 function handleMenu() {
     document.querySelector(".mainnav").classList.toggle("active");
-    // document.querySelector(".menu-trigger").classList.add("unvisible");
 }
 
 function closeMenu() {
-  console.log("kupa");
     document.querySelector(".mainnav").classList.remove("active");
-    // document.querySelector(".menu-trigger").classList.remove("unvisible");
-
 }
